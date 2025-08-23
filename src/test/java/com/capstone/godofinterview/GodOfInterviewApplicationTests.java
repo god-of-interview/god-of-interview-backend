697c6bf43dbc8857c4ba39fab38dfc5cba307554
dogyun00 <kimdk0903@naver.com>
2025-08-24 02:41:36 +0900

    Initial commit

---
+++ src/test/java/com/capstone/godofinterview/GodOfInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.godofinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodOfInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
