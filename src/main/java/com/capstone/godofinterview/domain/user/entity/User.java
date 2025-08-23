9ddeccf4de35023a6d2541334ea1049e16216a7d
dogyun00 <kimdk0903@naver.com>
2025-08-24 03:57:24 +0900

    feat: User 엔티티 구현

---
+++ src/main/java/com/capstone/godofinterview/domain/user/entity/User.java
@@ -0,0 +1,46 @@
+package com.capstone.godofinterview.domain.user.entity;
+
+import java.time.LocalDate;
+
+import com.capstone.godofinterview.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname; // 닉네임
+
+    @Column(nullable = false, unique = true)
+    private String email; // 로그인용 이메일
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String gender; // 성별
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String bio; // 자기소개
+
+    @Column(nullable = false)
+    private LocalDate birth; // 생년월일
+}
