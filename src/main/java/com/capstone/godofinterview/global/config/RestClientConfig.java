b2cf8d65b295f49eb44cb817ece470fbbdb2b7b0
dogyun00 <kimdk0903@naver.com>
2025-09-13 23:58:06 +0900

    refactor: RestClient 설정에서 FastAPI EC2 url로 재설정

--- src/main/java/com/capstone/godofinterview/global/config/RestClientConfig.java
+++ src/main/java/com/capstone/godofinterview/global/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.capstone.godofinterview.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -9,10 +10,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${fastapi.base-url}")
+    private String fastApiBaseUrl;
+
     @Bean
     public RestClient fastApiRestClient() {
         return RestClient.builder()
-            .baseUrl("http://localhost:8001")
+            .baseUrl(fastApiBaseUrl)
             .requestFactory(clientHttpRequestFactory())
             .build();
     }
